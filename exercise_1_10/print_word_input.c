a23fd9dc3d08c9922be16ab2c909e26297e00d7e
mled-v <matthewtledford@gmail.com>
2022-12-28 22:44:02 -0600

    exercises

---
+++ exercise_1_10/print_word_input.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#define YES 1
+#define NO 0
+
+int main(){
+
+    int c, is_word, not_word;
+
+    while((c = getchar()) != EOF){
+        if (c == '\n' || c == ' ' || c == '\t'){
+            putchar('\n');
+        }
+        else {
+            putchar(c);
+        }
+        }
+    }
